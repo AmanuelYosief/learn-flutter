1cff64ff17be91d9d721eb7903cc4bb2b97457f8
AmanuelYosief <43088213+AmanuelYosief@users.noreply.github.com>
2020-06-04 19:08:48 +0100

    WillPopScope to manage back (Software, Hardware) and sending a value back

--- flutter_start_3_navigation/lib/pages/products.dart
+++ flutter_start_3_navigation/lib/pages/products.dart
@@ -8,29 +8,40 @@ class ProductPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text(title),
-        ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Image.asset(imageUrl),
-            Container(
-              padding: EdgeInsets.all(10.0),
-              child: Text('Details!'),
-            ),
-            Container(
-              child: RaisedButton(
-                  padding: EdgeInsets.all(10.0),
-                  color: Theme.of(context).accentColor,
-                  //  Raised button that navigates back by routes
-                  //  (requires Context) to know it's whereabouts
-                  child: Text("DELETE"),
-                  onPressed: () => Navigator.pop(context, true)),
-            ),
-            Text('Details!'),
-          ],
-        ));
+    return WillPopScope(
+      onWillPop: () {
+        print("Back Button Pressed");
+        // Now have to manually program for the software
+        //  and hardware back button since we are blocking default
+        Navigator.pop(context, false);
+        return Future.value(
+            false); // If this is set to True, it deletes the other page aswell, hence false.
+            // Instead of null, now can decide and make use of back button returns. 
+      },
+      child: Scaffold(
+          appBar: AppBar(
+            title: Text(title),
+          ),
+          body: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Image.asset(imageUrl),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                child: Text('Details!'),
+              ),
+              Container(
+                child: RaisedButton(
+                    padding: EdgeInsets.all(10.0),
+                    color: Theme.of(context).accentColor,
+                    //  Raised button that navigates back by routes
+                    //  (requires Context) to know it's whereabouts
+                    child: Text("DELETE"),
+                    onPressed: () => Navigator.pop(context, true)),
+              ),
+              Text('Details!'),
+            ],
+          )),
+    );
   }
 }
